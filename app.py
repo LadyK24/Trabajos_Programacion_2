227350fa5a43734b6a05a0cd5d5c03f65350ee25
Alex111510 <163649841+Alex111510@users.noreply.github.com>
2024-04-14 18:07:47 -0500

    a

--- app.py
+++ app.py
@@ -1,16 +1,2 @@
-"""Conexión con GitHub"""
-
-""" Universidad Distrital Francisco José de Caldas"""
-
-""" Facultad tecnológica"""
-
-"""Ingeniería Civil"""
-
-"""Programación ll"""
-
-"""Lady Katterine Arévalo Becerra - 20231579020"""
-
-"""Daniel Felipe Castellanos Wilches - 20222579016"""
-
-"""Alexander Aranda Rodriguez - 20232579062"""
+import dash 
 
