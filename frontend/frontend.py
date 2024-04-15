a9ed36f7fe59aae237fad522c4d93aecaafc841a
Alex111510 <163649841+Alex111510@users.noreply.github.com>
2024-04-14 20:34:12 -0500

    a

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -5,8 +5,8 @@ import dash_bootstrap_components as dbc
 layout = dbc.Container([
     dbc.Row([
         dbc.Col('ENSAYO CORTE DIRECTO',md=80,style={'background-color':'orange'}),
-        dbc.Col('Descripción',md=30,style={'background-color':'orange'}),
-        dbc.Col('Datos',md=80,style={'background-color':'orange'}),
-        dbc.Col('Resultados',md=80,style={'background-color':'orange'}),
+        dbc.Col('Descripción',md=30,style={'background-color':'white'}),
+        dbc.Col('Datos',md=80,style={'background-color':'yellow'}),
+        dbc.Col('Resultados',md=80,style={'background-color':'blue'}),
     ])
 ])
