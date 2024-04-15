1338cc7ebe6b58c2021df94c1b362736e2fc2aab
Castellanos16 <wilchesdaniel00@gmail.com>
2024-04-14 22:25:30 -0500

    1

--- frontend/entradadatos/entradadatos.py
+++ frontend/entradadatos/entradadatos.py
@@ -2,11 +2,23 @@ import dash
 from dash import html
 import dash_bootstrap_components as dbc
 
+caracteristicas=dbc.Container([
+    html.H1("Características de la muestra"),
+    html.Button('Diametro'),
+    html.Button('Altura')
+])
+
+Datos_de_corte=dbc.Container([
+    html.H1("Datos de corte"),
+    html.Button('Sobre carga'),
+    html.Button('Carga nominal')
+])
+
 entrada_datos = dbc.Container([
     dbc.Row([
         dbc.Col('Ingreso de datos',md=12,style={'background-color':'yellow'}), 
-        dbc.Col('Característica de la muestra',md=6,style={'background-color':'orange'}),
-        dbc.Col('Datos de corte',md=6,style={'background-color':'gray'}),
+        dbc.Col(caracteristicas,md=6,style={'background-color':'orange'}),
+        dbc.Col(Datos_de_corte,md=6,style={'background-color':'gray'}),
         dbc.Col('Tabla con datos',md=12,style={'background-color':'green'}),
 
     ])
