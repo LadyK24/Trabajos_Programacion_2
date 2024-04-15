a7b6052ee82f31cfcfd7e3c7e8d5e72483dfb7a5
Alex111510 <163649841+Alex111510@users.noreply.github.com>
2024-04-14 21:04:54 -0500

    a

---
+++ frontend/descripción/descripción.py
@@ -0,0 +1,10 @@
+import dash 
+from dash import html
+import dash_bootstrap_components as dbc
+
+descripcion = dbc.Container([
+    dbc.Row([
+        dbc.Col('Descripción: Determinar la resitencia al corte de una muetra de suelo consolidado y drenado...',md=12,style={'background-color':'white'}),
+        
+    ])
+])
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,11 +3,12 @@ from dash import html
 import dash_bootstrap_components as dbc
 
 from frontend.titulo.titulo import titulo
+from frontend.descripción.descripción import descripcion
 
 layout = dbc.Container([
     dbc.Row([
         dbc.Col(titulo,md=12,style={'background-color':'orange'}),
-        dbc.Col('descripcion',md=12,style={'background-color':'white'}),
+        dbc.Col(descripcion,md=12,style={'background-color':'white'}),
         dbc.Col('entradadatos',md=6,style={'background-color':'yellow'}),
         dbc.Col('salidadatos',md=6,style={'background-color':'blue'}),
     ])
