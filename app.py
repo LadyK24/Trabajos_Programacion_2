c450081e558ccf797ba8cc53d1e526607c70e378
Alex111510 <163649841+Alex111510@users.noreply.github.com>
2024-04-14 19:15:44 -0500

    a

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+app.layout = dbc.Container([
+    html.H1('ENSAYO DE CORTE DIRECTO')
+])
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
 
 """algo"""
 """Calma"""
