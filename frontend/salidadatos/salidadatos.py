3d51675f41562e6009d9934c7dee2e4ed8a72a77
Castellanos16 <wilchesdaniel00@gmail.com>
2024-04-14 22:15:41 -0500

    1

--- frontend/salidadatos/salidadatos.py
+++ frontend/salidadatos/salidadatos.py
@@ -7,8 +7,8 @@ salida_datos = dbc.Container([
         dbc.Col('Curvas - Deformación horizontal',md=12,style={'background-color':'yellow'}), 
         dbc.Col('Tabla - Deformaciones y esfuerzo',md=6,style={'background-color':'orange'}),
         dbc.Col('Gráfica',md=6,style={'background-color':'gray'}),
-        dbc.Col('Esfuerzos normales y',md=12,style={'background-color':'green'}),
-        dbc.Col('Gráfica',md=12,style={'background-color':'pink'}),
+        dbc.Col('Esfuerzos normales y',md=6,style={'background-color':'green'}),
+        dbc.Col('Gráfica',md=6,style={'background-color':'pink'}),
         dbc.Col('Parámetros del suelo',md=12,style={'background-color':'purple'}),
 
     ])
