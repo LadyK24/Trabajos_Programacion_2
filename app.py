9fc76c20f19369cb414b9d22d91036c0d99835af
Alex111510 <163649841+Alex111510@users.noreply.github.com>
2024-04-14 20:23:54 -0500

    a

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import dash
 from dash import html
 import dash_bootstrap_components as dbc
 
+#importar el frontend
+from frontend.frontend import layout
+
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = dbc.Container([
-    html.H1('ENSAYO DE CORTE DIRECTO')
-])
+app.layout = layout
+
 if __name__ == '__main__':
     app.run_server(debug=True)
 
 
 
 
-"""algo"""
-"""Calma"""
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,3 +1,9 @@
 import dash 
 from dash import html
 import dash_bootstrap_components as dbc
+
+layout = dbc.Container([
+    dbc.Row([
+        dbc.Col('ENSAYO CORTE DIRECTO',md=20,style={'background-color':'orange'}),
+    ])
+])
