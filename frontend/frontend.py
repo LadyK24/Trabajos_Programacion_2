9a7a2b48d91d2a4f343debd8033fb7bbefd8e6c6
Castellanos16 <wilchesdaniel00@gmail.com>
2024-04-14 22:13:42 -0500

    1

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -5,13 +5,14 @@ import dash_bootstrap_components as dbc
 from frontend.titulo.titulo import titulo
 from frontend.descripción.descripción import descripcion
 from frontend.entradadatos.entradadatos import entrada_datos
-
+from frontend.salidadatos.salidadatos import salida_datos
 
 layout = dbc.Container([
     dbc.Row([
         dbc.Col(titulo,md=12,style={'background-color':'orange'}),
         dbc.Col(descripcion,md=12,style={'background-color':'white'}),
         dbc.Col(entrada_datos,md=6,style={'background-color':'yellow'}),
-        dbc.Col('salidadatos',md=6,style={'background-color':'blue'}),
+        dbc.Col(salida_datos,md=6,style={'background-color':'blue'}),
+    
     ])
 ])
