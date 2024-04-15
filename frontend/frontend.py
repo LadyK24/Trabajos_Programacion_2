a8b39057ef376487321be43e335ce373bf8c48fc
Alex111510 <163649841+Alex111510@users.noreply.github.com>
2024-04-14 20:57:51 -0500

    a

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -6,7 +6,7 @@ from frontend.titulo.titulo import titulo
 
 layout = dbc.Container([
     dbc.Row([
-        dbc.Col('titulo',md=12,style={'background-color':'orange'}),
+        dbc.Col(titulo,md=12,style={'background-color':'orange'}),
         dbc.Col('descripcion',md=12,style={'background-color':'white'}),
         dbc.Col('entradadatos',md=6,style={'background-color':'yellow'}),
         dbc.Col('salidadatos',md=6,style={'background-color':'blue'}),
