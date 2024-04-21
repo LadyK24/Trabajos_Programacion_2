65ad010da118cf1b3b9391029783eec60542ed12
Lady Katterine Arévalo Becerra <161129932+LadyK24@users.noreply.github.com>
2024-04-21 11:38:38 -0500

    L

--- frontend/entradadatos/entradadatos.py
+++ frontend/entradadatos/entradadatos.py
@@ -1,5 +1,5 @@
-import dash 
-from dash import html
+import dash
+from dash import Dash, dcc, html, Input, Output, callback, dash_table
 import dash_bootstrap_components as dbc
 
 ingresodatos=dbc.Container([
@@ -23,12 +23,26 @@ Datos_de_corte=dbc.Container([
     html.Button('Carga nominal')
 ])
 
+
+tabla_datos=dbc.Container([
+    html.H3("Igrese sus datos en deformación y fueza"),
+    dash_table.DataTable(
+        id= 'tabla_corte_directo',
+        columns=[
+            {'name':'Deformación','id':'Deformación','editable':False},
+        ],
+        #data=
+    )
+
+])
+
+
 entrada_datos = dbc.Container([
     dbc.Row([
         dbc.Col(ingresodatos,md=12,style={'background-color':'yellow'}), 
         dbc.Col(caracteristicas,md=6,style={'background-color':'orange'}),
         dbc.Col(Datos_de_corte,md=6,style={'background-color':'gray'}),
-        dbc.Col('Tabla con datos',md=12,style={'background-color':'green'}),
+        dbc.Col(tabla_datos,md=12,style={'background-color':'green'}),
 
     ])
 ])
